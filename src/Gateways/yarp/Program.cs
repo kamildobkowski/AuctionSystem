310470100d4760b3480b60a71f538c343ba8add1
kamildobkowski <kamil.dobkowski.03@gmail.com>
2025-05-04 16:24:40 +0200

    Add cors to gateway

--- src/Gateways/yarp/Program.cs
+++ src/Gateways/yarp/Program.cs
@@ -1,10 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(o => o.AddPolicy("AllowAll", p =>
+	p.AllowAnyOrigin()
+		.AllowAnyHeader()
+		.AllowAnyMethod()
+));
+
 builder.Services.AddReverseProxy()
 	.LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
 
+app.UseCors("AllowAll");
 app.MapReverseProxy();
-
 app.Run();
